020701f82ab6a0335ed3c782c0b5c4f59c5247e9
vladimir.tischenko <vladimir.tischenko@oracle.com>
2016-12-23 17:49:40 +0200

    structure created

---
+++ webpack.config.js
@@ -0,0 +1,91 @@
+"use strict";
+
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const poststylus = require('poststylus');
+const rucksack = require('rucksack-css');
+const stylusLoader = ExtractTextPlugin.extract("style-loader", "css-loader?minimize!stylus-loader");
+const NODE_ENV = process.env.NODE_ENV || "development";
+
+let config = [{
+    name: 'js',
+    entry: {
+        app: './public/js/app.js',
+    },
+    output: {
+        path: "./public/build/",
+        filename: 'build.[name].js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js?$/,
+                exclude: /(node_modules)/,
+                loader: "babel",
+                query: {
+                    presets: ['es2015']
+                }
+            }
+        ]
+    },
+    resolve: {
+        modulesDirectories: ["node_modules"],
+        extensions: ["", ".js", "css", "styl", "woff", "ttf", "otf", "jpg"]
+	}
+}, {
+    name: 'styles',
+    entry: {
+        styles: "./public/styl/build.styl",
+    },
+    exclude: '/node_modules/',
+    output: {
+        path: './public/build/',
+        filename: '[name].min.css'
+    },
+    module: {
+       loaders: [
+            {
+                test: /\.styl$/,
+                loader: stylusLoader
+            },
+            {
+                test: /\.(jpg|png|woff|woff2|eot|ttf|svg|otf)$/, 
+                loader: 'url-loader?limit=100000'
+            }
+        ]
+    },
+    stylus: {
+      use: [
+        poststylus(rucksack({
+          autoprefixer: true
+        }))
+      ]
+    },
+    plugins: [
+        new ExtractTextPlugin("[name].min.css")
+    ]
+}, {
+    name: 'jade',
+    entry: {
+        index: "./public/jade/index.jade"
+    },
+    exclude: '/node_modules/',
+    output: {
+        path: './',
+        filename: '[name].html'
+    },
+    module: {
+       loaders: [
+            { 
+                test: /\.jade$/, 
+                loader: 'jade' 
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin("[name].html");
+    ]
+}];
+
+
+module.exports = config;
